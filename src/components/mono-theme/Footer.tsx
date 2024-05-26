0acea73bdaf8708430d995b5103023b593d6fc1b
csvidit <63170797+csvidit@users.noreply.github.com>
2024-05-26 16:20:01 -0400

    improve pagespeed scores

--- src/components/mono-theme/Footer.tsx
+++ src/components/mono-theme/Footer.tsx
@@ -1,11 +1,11 @@
 const Footer = () => {
     return (
       <footer className="col-span-4 flex flex-row space-x-4 items-center row-span-1 bg-transparent text-slate-500 *:uppercase">
-        <div className="w-fit">
+        <div className="">
           &copy; {new Date().getFullYear()} Vidit Khandelwal / All rights reserved /
           All wrongs reserved, too
         </div>
-        <div className="grow border-b border-b-gray-600" />
+        {/* <div className="grow border-b border-b-gray-600" /> */}
       </footer>
     );
   };
