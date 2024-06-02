fa42548f6cbcb4f11ef25279fb15ec50c2cee8d6
csvidit <63170797+csvidit@users.noreply.github.com>
2024-06-02 17:53:32 -0400

    fix build errors

--- src/components/Spotify.tsx
+++ src/components/Spotify.tsx
@@ -1,20 +1,20 @@
-const getData = async () => {
-  const tokenResponse = await fetch(
-    `https://api.spotify.com/v1/me/player/currently-playing`,
-    {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    },
-  );
-  const tokenData = await tokenResponse.json();
-  const accessToken = tokenData.access_token;
-};
+// const getData = async () => {
+//   const tokenResponse = await fetch(
+//     `https://api.spotify.com/v1/me/player/currently-playing`,
+//     {
+//       headers: {
+//         Authorization: `Bearer ${accessToken}`,
+//       },
+//     },
+//   );
+//   const tokenData = await tokenResponse.json();
+//   const accessToken = tokenData.access_token;
+// };
 
-const Spotify = () => {
-  return (
-    <div className="col-span-4 h-fit w-full rounded-lg bg-gray-50 p-4">
-      <div>Last played: </div>
-    </div>
-  );
-};
+// const Spotify = () => {
+//   return (
+//     <div className="col-span-4 h-fit w-full rounded-lg bg-gray-50 p-4">
+//       <div>Last played: </div>
+//     </div>
+//   );
+// };
--- src/utils/spotify.config.ts
+++ src/utils/spotify.config.ts
@@ -1,12 +1,12 @@
-import { SpotifyWebApi } from "@spotify/web-api-ts-sdk";
+// import { SpotifyWebApi } from "@spotify/web-api-ts-sdk";
 
-// Choose one of the following:
-const sdk = SpotifyApi.withUserAuthorization(
-  "client-id",
-  "https://localhost:3000",
-  ["scope1", "scope2"],
-);
-const sdk = SpotifyApi.withClientCredentials("client-id", "secret", [
-  "scope1",
-  "scope2",
-]);
+// // Choose one of the following:
+// const sdk = SpotifyApi.withUserAuthorization(
+//   "client-id",
+//   "https://localhost:3000",
+//   ["scope1", "scope2"],
+// );
+// const sdk = SpotifyApi.withClientCredentials("client-id", "secret", [
+//   "scope1",
+//   "scope2",
+// ]);
