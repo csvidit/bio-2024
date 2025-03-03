7fc716eba29c04be2c38e7a88a7d554064c1b105
csvidit <63170797+csvidit@users.noreply.github.com>
2025-03-03 13:06:07 -0500

    (neon-dark-theme/sections/Intro.tsx) add spotify now playing UI

--- src/components/neon-dark-theme/sections/Intro.tsx
+++ src/components/neon-dark-theme/sections/Intro.tsx
@@ -1,56 +1,70 @@
-import { Briefcase, Element, Location, PresentionChart } from "iconsax-react";
 import Link from "next/link";
 import {
   PiArrowUpRightBold,
   PiBriefcaseFill,
   PiMapPinFill,
   PiPresentationChartFill,
+  PiSpotifyLogoFill,
 } from "react-icons/pi";
 import SectionContainer from "../containers/SectionContainer";
 import FeaturedLinks from "../links/FeaturedLinks";
-import styles from "../Pride.module.css"
+import { getNowPlaying } from "@/actions/spotify";
 
-const Intro = () => {
+const Intro = async () => {
+  const song = await getNowPlaying();
+
   return (
     <SectionContainer>
       <div className="flex flex-col items-start gap-4 lg:flex-row lg:gap-8">
-        <div className="flex w-full flex-col text-sm text-neutral-400 lg:w-2/3">
-          <div className="text-lg text-white lg:text-xl">Vidit Khandelwal</div>
-          <span className="text-neutral-500">
-            full stack engineer fueled by web dev, AI, and a splash of mobile
-            dev.
-          </span>
+        <div className="flex flex-col gap-4">
+          <div className="flex w-full flex-col text-sm text-neutral-400 lg:w-2/3">
+            <div className="text-lg text-white lg:text-xl">
+              Vidit Khandelwal
+            </div>
+            <span className="text-neutral-500">
+              full stack engineer fueled by web dev, AI, and a splash of mobile
+              dev.
+            </span>
+          </div>
+          {song.isPlaying && (
+            <div>
+              <span className="inline-flex items-center gap-1 *:text-xs">
+                <PiSpotifyLogoFill
+                  className={`inline shrink-0 ${song?.isPlaying ? "animate-pulse text-lime-400" : "text-neutral-600"}`}
+                />{" "}
+                <span className="inline text-neutral-300">
+                  {!song || !song.isPlaying ? (
+                    <>Not playing</>
+                  ) : (
+                    <>
+                      {song.title}{" "}
+                      <span className="text-neutral-500">
+                        &mdash; {song.artist}
+                      </span>
+                    </>
+                  )}
+                </span>
+              </span>
+            </div>
+          )}
         </div>
-
         <div className="flex w-full flex-col gap-1 text-sm lg:w-1/3">
           <span className="inline-flex items-center gap-1">
-            <PiMapPinFill
-              //   size={16}
-              //   variant="Bulk"
-              className="inline text-neutral-600"
-            />{" "}
+            <PiMapPinFill className="inline text-neutral-600" />{" "}
             <span className="inline text-neutral-300">United States</span>
           </span>
           <span className="inline-flex items-center gap-1">
-            <PiBriefcaseFill
-              //   size={16}
-              //   variant="Bulk"
-              className="inline text-neutral-600"
-            />{" "}
+            <PiBriefcaseFill className="inline text-neutral-600" />{" "}
             <span className="inline text-neutral-300">Open To Work</span>
           </span>
           <Link
             href="https://viditkhandelwal.com"
             className="group inline-flex items-center gap-1 transition-all duration-200 ease-in-out"
           >
-            <PiPresentationChartFill
-              //   size={16}
-              //   variant="Bulk"
-              className="inline text-neutral-600 transition-all duration-200 ease-in-out group-hover:-rotate-12 group-hover:scale-105 group-hover:transform group-hover:text-lime-400"
-            />
+            <PiPresentationChartFill className="inline text-neutral-600 transition-all duration-200 ease-in-out group-hover:-rotate-12 group-hover:scale-105 group-hover:transform group-hover:text-lime-400" />
             <span className="group-hover:text-citron-400 inline text-neutral-300 transition-all duration-200 ease-in-out">
               Portfolio{" "}
-              <PiArrowUpRightBold className="text-lime-400 group-hover:text-lime-400 transition-all duration-200 ease-in-out inline group-hover:transform group-hover:scale-125" />
+              <PiArrowUpRightBold className="inline text-lime-400 transition-all duration-200 ease-in-out group-hover:scale-125 group-hover:transform group-hover:text-lime-400" />
             </span>
           </Link>
         </div>
@@ -59,5 +73,4 @@ const Intro = () => {
     </SectionContainer>
   );
 };
-
 export default Intro;
